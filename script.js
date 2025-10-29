2bea27f8072df55c9dcd491582dda6fdd3452031
ShyShy <diegoferrm19@gmail.com>
2025-10-28 21:13:16 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const mudacor = document.getElementById('mod-escu');
+const mudacor = document.getElementById('mod-escuro');
 const body = document.body;
+
 
 mudacor.addEventListener("click" , () => {body.classList.toggle('dark-mode')})
